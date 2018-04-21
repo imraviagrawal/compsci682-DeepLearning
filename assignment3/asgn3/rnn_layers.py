16e335a86b99adc158d02c59bf4623aa4cfbaa72
imraviagrawal <raviagrawal@umass.edu>
2018-04-21 10:47:18 -0400

    rnn backward and word embedding tested

--- assignment3/asgn3/rnn_layers.py
+++ assignment3/asgn3/rnn_layers.py
@@ -142,13 +142,14 @@ def rnn_backward(dh, cache):
   x, h, h0, Wh, Wx, b = cache
  
   N, T, H = dh.shape
+  next_h_shape = (N, H)
   D = Wx.shape[0]
   dx = np.zeros_like(x)
   dh0 = np.zeros_like(h0)
   dWx = np.zeros_like(Wx)
   dWh = np.zeros_like(Wh)
   db = np.zeros_like(b)
-  dprev_h = np.zeros((N, H))
+  dprev_h = np.zeros(next_h_shape)
   
   next_h = h[:, T-1, :]
 
@@ -160,11 +161,9 @@ def rnn_backward(dh, cache):
       prev_h = h[:, t_vector - 1, :]
     else:
       prev_h = h0
-    
     dnext_h = dh[:, t_vector, :] + dprev_h
     dx[:, t_vector, :], dprev_h, dWx_t, dWh_t, db_t = rnn_step_backward(dnext_h, (b, next_h, prev_h, Wx, Wh, x_t))
-    next_h = prev_h
-    dWx += dWx_t; dWh += dWh_t; db += db_t
+    dWx += dWx_t; dWh += dWh_t; db += db_t; next_h = prev_h
   
   dh0 = dprev_h
   ##############################################################################
@@ -196,11 +195,13 @@ def word_embedding_forward(x, W):
   ##############################################################################
   N, T = x.shape
   V, D = W.shape
-  out = np.zeros((N, T, D))
+  out_shape = (N, T, D)
+  out = np.zeros(out_shape)
   
   for n in xrange(N):
     for t in xrange(T):
-      out[n, t] = W[x[n, t]]
+      W_index = x[n, t]
+      out[n, t] = W[ W_index]
   cache = (x, W)
   ##############################################################################
   #                               END OF YOUR CODE                             #
